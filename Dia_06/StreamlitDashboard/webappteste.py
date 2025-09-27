8f495b12882485b2ded1fc15d2014265af2c0d61
Diego Rodrigues <diegomotta93@gmail.com>
2025-09-27 16:21:40 -0300

    Update webappteste.py

--- Dia_06/StreamlitDashboard/webappteste.py
+++ Dia_06/StreamlitDashboard/webappteste.py
@@ -1,9 +1,2 @@
 import streamlit as st
-
-st.header('st.selectbox')
-
-option = st.selectbox(
-     'What is your favorite color?',
-     ('Blue', 'Red', 'Green'))
-
-st.write('Your favorite color is ', option)
+st.title ("teste")
